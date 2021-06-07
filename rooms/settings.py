abcce1ed6101f9e288d3f0726688b3c817d8f344
Elias Aquino <38292592+eliasti@users.noreply.github.com>
2021-06-07 20:00:06 -0300

    Update settings.py

--- rooms/settings.py
+++ rooms/settings.py
@@ -179,7 +179,7 @@ if os.getcwd() == '/app':
 
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['reservei.herokuapp.com']
     
 
     DEBUG = True
